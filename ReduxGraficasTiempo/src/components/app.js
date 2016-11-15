4f29141d38ffacee0df72b0b4fcceb4eb4ee0f5e
ncalero <ncalero@gmail.com>
2016-11-15 21:30:08 +0100

    Inicio ejercicio curso

---
+++ ReduxGraficasTiempo/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import SearchBar from '../containers/search_  bar'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <SearchBar />      
+      </div>
+    );
+  }
+}
